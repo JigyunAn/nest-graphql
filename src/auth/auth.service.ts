b479dbadfcd6deb5cd4504db9f1ead5bcb76d03b
mokodoko <forever7631@gmail.com>
2022-12-25 23:06:45 +0900

    인증모듈 로그 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,26 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { User } from 'src/user/entities/user.entity';
+import { Logger as WinLog } from 'winston';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+
 @Injectable()
 export class AuthService {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    @Inject(WINSTON_MODULE_PROVIDER)
+    private readonly log: WinLog,
+  ) {}
 
   getToken(loginUser: User): string {
     try {
       const payload = { email: loginUser.email, idx: loginUser.idx };
       return this.jwtService.sign(payload);
     } catch (err) {
-      throw new UnauthorizedException();
+      this.log.error('[getToken]', {
+        inputData: loginUser,
+        error: err,
+      });
     }
   }
 
@@ -20,7 +30,10 @@ export class AuthService {
 
       return payload;
     } catch (err) {
-      throw new UnauthorizedException();
+      this.log.error('[verify]', {
+        inputData: jwtString,
+        error: err,
+      });
     }
   }
 }
--- src/auth/jwt/jwt.strategy.ts
+++ src/auth/jwt/jwt.strategy.ts
@@ -1,10 +1,17 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { UserService } from 'src/user/user.service';
+import { Logger as WinLog } from 'winston';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly usersService: UserService) {
+  constructor(
+    private readonly usersService: UserService,
+    @Inject(WINSTON_MODULE_PROVIDER)
+    private readonly log: WinLog,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -17,7 +24,10 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     if (user) {
       return user;
     } else {
-      throw new UnauthorizedException('User Not Found');
+      this.log.error('[validate]', {
+        inputData: payload,
+        error: '유요하지않은 토큰입니다.',
+      });
     }
   }
 }
